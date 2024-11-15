0c0154f9e7951deb5a2cc93e5d8e7f77c25fc8a8
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2024-11-15 15:45:54 +0800

    add spline case?

---
+++ honda_flux_handling3D_spline.cxx
@@ -0,0 +1,197 @@
+#include "hkkm_reader_3D.hxx"
+#include "interpolation_spline.hxx"
+#include <TAttLine.h>
+#include <algorithm>
+#include <boost/program_options.hpp>
+#include <cmath>
+#include <print>
+
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TF2.h>
+#include <TStyle.h>
+
+int main(int argc, char **argv) {
+  namespace po = boost::program_options;
+  po::options_description desc("Allowed options");
+  desc.add_options()("help", "produce help message")                       //
+      ("input-file,i", po::value<std::string>()->required(), "input file") //
+      ("fix-E,e", po::value<double>()->default_value(1.6), "fixed energy") //
+      ("fix-costh,c", po::value<double>()->default_value(0.85),
+       "fixed costh") //
+      ("fix-phi,p", po::value<double>()->default_value(1.0),
+       "fixed phi") //
+      ("output,o", po::value<std::string>()->default_value(""),
+       "Prefix For output plots");
+  po::variables_map vm;
+  try {
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << '\n';
+    std::cerr << desc << '\n';
+    return 1;
+  }
+  if (vm.contains("help")) {
+    std::cout << desc << '\n';
+    return 1;
+  }
+  auto input_file = vm["input-file"].as<std::string>();
+  const double fixed_costh = vm["fix-costh"].as<double>();
+  const double fixed_energy = vm["fix-E"].as<double>();
+  const double fixed_phi = vm["fix-phi"].as<double>();
+
+  HKKM_READER_3D reader(input_file);
+  auto &numu = reader[14];
+  constexpr size_t n_logE_points = 101;
+  constexpr size_t n_costh_bins = 20;
+  constexpr size_t n_costh_points = n_costh_bins + 1;
+  constexpr size_t n_phi_bins = 12;
+  constexpr size_t n_phi_points = n_phi_bins + 1;
+
+  // here is where the interpolation method initializes
+  axis_object logE_points{.min = -1, .max = 4, .n_points = n_logE_points};
+  axis_object costh_points{.min = -1, .max = 1, .n_points = n_costh_points};
+  axis_object phi_points{
+      .min = 0, .max = 2 * TMath::Pi(), .n_points = n_phi_points};
+
+  interpolate<3> interp{{logE_points, costh_points, phi_points}};
+  for (size_t i = 0; i < n_logE_points; ++i) {
+    interp[{i, 0, 0}] = 0;
+    for (size_t j = 0; j < n_costh_bins; ++j) {
+      interp[{i, j + 1, 0}] = 0;
+      for (size_t k = 0; k < n_phi_bins; ++k) {
+        interp[{i, 0, k + 1}] = 0;
+        interp[{i, j + 1, k + 1}] = numu.GetBinContent(i + 1, j + 1, k + 1) *
+                                    0.1 * (M_PI * 2 / n_phi_bins);
+        interp[{i, j + 1, k + 1}] += interp[{i, j + 1, k}];
+      }
+      for (size_t k = 0; k < n_phi_bins; ++k) {
+        interp[{i, j + 1, k + 1}] += interp[{i, j, k + 1}];
+      }
+    }
+  }
+
+  // then plot
+  auto get_flux = [&](double E, double costh, double phi) {
+    auto logE = std::log10(E);
+    return interp.do_interpolation({logE, costh, phi}, {false, true, true});
+  };
+  auto get_flux_no_interop = [&](double E, double costh, double phi) {
+    auto logE = std::log10(E);
+    auto binx = numu.GetXaxis()->FindBin(logE);
+    auto biny = numu.GetYaxis()->FindBin(costh);
+    auto binz = numu.GetZaxis()->FindBin(phi);
+    return numu.GetBinContent(binx, biny, binz);
+  };
+
+  // double interpolated = get_flux(fixed_energy, fixed_costh, fixed_phi);
+  // double non_interpolated =
+  //     get_flux_no_interop(fixed_energy, fixed_costh, fixed_phi);
+  // std::println("Interpolated: {}, Non-interpolated: {}", interpolated,
+  //              non_interpolated);
+
+  // TApplication app("app", nullptr, nullptr);
+  constexpr int res_factor = 1;
+  TCanvas c1("c1", "c1", 800 * 3 * res_factor, 600 * res_factor);
+  c1.Divide(3, 1);
+  c1.cd(1);
+
+  TF1 flux_given_costh{
+      "flux",
+      [&](double *x, double *) {
+        return std::log(get_flux(std::pow(10, x[0]), fixed_costh, fixed_phi));
+      },
+      -1, 4, 0};
+  flux_given_costh.SetTitle(
+      std::format("Flux along cos #theta = {:.1f},  #phi = {:.1f}; "
+                  "log_{{10}}(E/GeV); ln (#Phi/(m^{{2}} sec sr GeV) ",
+                  fixed_costh, fixed_phi)
+          .c_str());
+  // flux_given_costh.SetNpx(1000);
+  const double costh_thres = 1.0;
+  TF1 flux_given_E{"flux",
+                   [&](double *x, double *) {
+                     return get_flux(fixed_energy, x[0], fixed_phi);
+                   },
+                   -costh_thres, costh_thres, 0};
+  // flux_given_E.SetNpx(1000);
+  flux_given_E.SetTitle(std::format("Flux along E = {} GeV,  #phi = {:.1f}; "
+                                    "cos #theta; #Phi (m^{{2}} sec sr GeV)",
+                                    fixed_energy, fixed_phi)
+                            .c_str());
+
+  TF1 flux_given_costh_no_interop{
+      "flux",
+      [&](double *x, double *) {
+        return std::log(
+            get_flux_no_interop(std::pow(10, x[0]), fixed_costh, fixed_phi));
+      },
+      -1, 4, 0};
+  // flux_given_costh_no_interop.SetNpx(1000);
+  TF1 flux_given_E_no_interop{"flux",
+                              [&](double *x, double *) {
+                                return get_flux_no_interop(fixed_energy, x[0],
+                                                           fixed_phi);
+                              },
+                              -costh_thres, costh_thres, 0};
+  // flux_given_E_no_interop.SetNpx(1000);
+
+  TF1 flux_given_E_costh{"flux",
+                         [&](double *x, double *) {
+                           //  return std::log(
+                           return get_flux(fixed_energy, fixed_costh, x[0]);
+                         },
+                         0, 2 * M_PI, 0};
+  // flux_given_E_costh.SetNpx(1000);
+  flux_given_E_costh.SetTitle(
+      std::format("Flux along E = {} GeV, cos  #theta = "
+                  "{:.1f}; #phi; #Phi (m^{{2}} sec sr GeV)",
+                  fixed_energy, fixed_costh)
+          .c_str());
+  TF1 flux_given_E_costh_no_interop{"flux",
+                                    [&](double *x, double *) {
+                                      return get_flux_no_interop(
+                                          fixed_energy, fixed_costh, x[0]);
+                                    },
+                                    0, 2 * M_PI, 0};
+  // flux_given_E_costh_no_interop.SetNpx(1000);
+
+  auto do_plot = [&](int id, TF1 &f, TF1 &f_no_interop) {
+    f.SetNpx(500);
+    f_no_interop.SetNpx(1000);
+    auto xmin = f.GetXmin();
+    auto xmax = f.GetXmax();
+    xmin = xmin + 0.01 * (xmax - xmin);
+    xmax = xmax - 0.01 * (xmax - xmin);
+
+    auto min =
+        std::min(f.GetMinimum(xmin, xmax), f_no_interop.GetMinimum(xmin, xmax));
+    auto max =
+        std::max(f.GetMaximum(xmin, xmax), f_no_interop.GetMaximum(xmin, xmax));
+    min = min - 0.1 * std::abs(max - min);
+    max = max + 0.1 * std::abs(max - min);
+
+    f.SetMinimum(min);
+    f.SetMaximum(max);
+    c1.cd(id);
+    f.Draw();
+    f_no_interop.SetLineColor(kBlue);
+    f_no_interop.SetLineStyle(kDotted);
+    f_no_interop.SetLineWidth(3);
+    f_no_interop.Draw("same");
+  };
+
+  do_plot(1, flux_given_costh, flux_given_costh_no_interop);
+  do_plot(2, flux_given_E, flux_given_E_no_interop);
+  do_plot(3, flux_given_E_costh, flux_given_E_costh_no_interop);
+
+  c1.Draw();
+  auto output_str = vm["output"].as<std::string>();
+  if (output_str.size()) {
+    c1.SaveAs(std::format("{}_flux.svg", output_str).c_str());
+    c1.SaveAs(std::format("{}_flux.pdf", output_str).c_str());
+    c1.SaveAs(std::format("{}_flux.eps", output_str).c_str());
+  }
+  // app.Run();
+}
---
+++ include/interpolation_spline.hxx
@@ -0,0 +1,157 @@
+#include <array>
+#include <cmath>
+#include <cstddef>
+#include <numeric>
+#include <print>
+#include <vector>
+
+#include <TSpline.h>
+
+#include <Eigen/Dense>
+
+struct axis_object {
+  double min{};
+  double max{};
+  size_t n_points{};
+};
+
+// template parameter specifies the order of the polynomial
+// to use for interpolation for each axis
+template <size_t dimension> class interpolate {
+public:
+  // static constexpr size_t dimension = sizeof...(poly_orders);
+  // static constexpr std::array<size_t, dimension> orders_array = {
+  //     (poly_orders + 1)...};
+  constexpr static size_t npoints = 5;
+
+  interpolate(const std::array<axis_object, dimension> &axes_)
+      : axes(axes_), values([](const auto &axes) {
+          size_t n_values = 1;
+          for (const auto &axis : axes) {
+            n_values *= axis.n_points;
+          }
+          return std::vector<double>(n_values);
+        }(axes_)) {}
+
+  interpolate(const interpolate &) = default;
+  interpolate(interpolate &&) = default;
+  interpolate &operator=(const interpolate &) = default;
+  interpolate &operator=(interpolate &&) = default;
+  ~interpolate() = default;
+
+  auto &operator[](const std::array<size_t, dimension> &index_array) {
+    size_t flat_index = 0;
+    for (size_t i = 0; i < dimension; ++i) {
+      flat_index *= axes[i].n_points;
+      flat_index += index_array[i];
+    }
+    return values[flat_index];
+  }
+
+  const auto &
+  operator[](const std::array<size_t, dimension> &index_array) const {
+    size_t flat_index = 0;
+    for (size_t i = 0; i < dimension; ++i) {
+      flat_index *= axes[i].n_points;
+      flat_index += index_array[i];
+    }
+    return values[flat_index];
+  }
+
+  double do_interpolation(
+      const std::array<double, dimension> &coordinates,
+      const std::array<bool, dimension> &do_derivative = {}) const {
+    auto grid = build_grid(coordinates, do_derivative);
+    auto value = do_interpolation_from_grid(grid);
+    for (size_t i = 0; i < dimension; ++i) {
+      if (do_derivative[i]) {
+        value /= (axes[i].max - axes[i].min) / (axes[i].n_points - 1) *
+                 (npoints - 1) / 2;
+      }
+    }
+    return value;
+  }
+
+private:
+  std::array<axis_object, dimension> axes;
+  std::vector<double> values;
+
+  template <size_t dimension_index = 0>
+  auto build_grid(const std::array<double, dimension> &coordinates,
+                  const std::array<bool, dimension> &do_derivative,
+                  const std::array<size_t, dimension_index> &index =
+                      std::array<size_t, 0>{}) const {
+    constexpr size_t order = npoints;
+    double normalized_coordinates =
+        (coordinates[dimension_index] - axes[dimension_index].min) /
+        (axes[dimension_index].max - axes[dimension_index].min) *
+        (axes[dimension_index].n_points - 1);
+    auto closest_indices = get_closest_indices(normalized_coordinates,
+                                               axes[dimension_index].n_points);
+    // notice this value is defined in [-1, 1]
+    auto normalized_value =
+        -1 + (2 * (normalized_coordinates - closest_indices[0]) /
+              (closest_indices[order - 1] - closest_indices[0]));
+    auto this_do_derivative = do_derivative[dimension_index];
+    std::array<size_t, dimension_index + 1> new_coord{};
+    for (size_t i = 0; i < dimension_index; ++i) {
+      new_coord[i] = index[i];
+    }
+    if constexpr (dimension_index == dimension - 1) {
+      std::array<double, order> sub_grids;
+      for (size_t i = 0; i < order; ++i) {
+        new_coord[dimension_index] = closest_indices[i];
+        sub_grids[i] = (*this)[new_coord];
+      }
+      return std::make_tuple(sub_grids, this_do_derivative, normalized_value);
+    } else {
+      using sub_grid_type =
+          decltype(build_grid(coordinates, do_derivative,
+                              std::array<size_t, dimension_index + 1>{}));
+      std::array<sub_grid_type, order> ret{};
+      for (size_t i = 0; i < order; ++i) {
+        new_coord[dimension_index] = closest_indices[i];
+        ret[i] = build_grid(coordinates, do_derivative, new_coord);
+      }
+      return std::make_tuple(ret, this_do_derivative, normalized_value);
+    }
+  }
+
+  template <class T>
+  static double do_interpolation_from_grid(
+      const std::tuple<std::array<T, npoints>, bool, double> &grid_values) {
+    const auto &[sub_grids, do_derivative, value] = grid_values;
+    std::array<double, npoints> t_value, yvalue;
+    for (size_t i = 0; i < npoints; ++i) {
+      if constexpr (std::is_same_v<T, double>) {
+        yvalue[i] = sub_grids[i];
+      } else {
+        yvalue[i] = do_interpolation_from_grid(sub_grids[i]);
+      }
+      t_value[i] = -1 + 2. * i / (npoints - 1);
+    }
+    TSpline3 spline{"spline", t_value.data(), yvalue.data(), npoints};
+    if (do_derivative) {
+      return spline.Derivative(value);
+    }
+    return spline.Eval(value);
+  }
+
+  static std::array<size_t, npoints> get_closest_indices(double this_index_d,
+                                                         size_t n_points) {
+    std::array<size_t, npoints> ret{};
+    std::iota(ret.begin(), ret.end(), 0);
+    auto mid = (npoints - 1) / 2.0;
+    auto shift = std::round(this_index_d - mid);
+    if (shift < 0) {
+      shift = 0;
+    } else if (shift > n_points - npoints) {
+      shift = n_points - npoints;
+    }
+
+    for (auto &i : ret) {
+      i += shift;
+    }
+    return ret;
+  }
+};
